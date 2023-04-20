6106319dd55b7a90a9bd54aeabfb67df4d8183a4
Steve Larsen <slarsen-3@att.net>
2023-04-20 18:08:02 -0500

    test for lowestArrayAverage

--- lib/src/test/java/basiclibrary/LibraryTest.java
+++ lib/src/test/java/basiclibrary/LibraryTest.java
@@ -48,4 +48,23 @@ class LibraryTest {
         // Assert
         assertEquals(expected, actual);
     }
+
+    @Test
+    void testLowestAverageArray() {
+        // Arrange
+        Library sut = new Library();
+        int[][] array = {
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        };
+        int[] expected = {55, 54, 60, 53, 59, 57, 61};
+
+        // Act
+        int[] actual = sut.lowestAverageArray(array);
+
+        // Assert
+        assertArrayEquals(expected, actual);
+    }
 }
